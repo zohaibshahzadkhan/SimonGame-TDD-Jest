f354d5dac998a1662554784149668c8b25c5d567
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-04-17 14:47:23 +0200

    test: Add failing test for game object

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import fs from "fs";
+import game from "../game.js";
+
+beforeAll(() => {
+  let fileContent = fs.readFileSync("index.html", "utf-8")
+  document.open()
+  document.write(fileContent)
+  document.close()
+})
+
+describe("Game object contain correct keys", () => {
+  test("score key exists", () => {
+    expect("score" in game).toBe(true)
+  })
+  test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true)
+  })
+  test("playerMoves key exists", () => {
+    expect("playerMoves" in game).toBe(true)
+  })
+  test("choices key exists", () => {
+    expect("choices" in game).toBe(true)
+  })
+  test("choices should contain correct id's", () => {
+    expect(game.choices).toEqual(['button1', 'button2', 'button3', 'button4'])
+  })
+})
