43b932e72b4e6681cb17b5d7b22583747e57859c
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-04-17 23:31:14 +0200

    functionality: Add showTurns function to pass tests

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,8 @@ const game = {
   score: 0,
   currentGame: [],
   playerMoves: [],
-  choices: ['button1', 'button2', 'button3', 'button4']
+  choices: ['button1', 'button2', 'button3', 'button4'],
+  turnNumber: 0
 }
 const newGame = () => {
   game.score = 0
@@ -19,6 +20,7 @@ const showScore = () => {
 const addTurn = () => {
   game.playerMoves = []
   game.currentGame.push(game.choices[Math.floor(Math.random() * 4)])
+  showTurns()
 }
 
 const lightsOn = (circleId) => {
@@ -28,10 +30,21 @@ const lightsOn = (circleId) => {
   }, 4000);
 }
 
+const showTurns = () => {
+  game.turnNumber = 0
+  let turn = setInterval(() => {
+    lightsOn(game.currentGame[game.turnNumber])
+    game.turnNumber++
+    if(turnNumber >= game.currentGame.length){
+      clearInterval(turn)
+    }
+  }, 800);
+}
 
 export {
   game,
   newGame,
   addTurn,
-  lightsOn
+  lightsOn,
+  showTurns
 }
